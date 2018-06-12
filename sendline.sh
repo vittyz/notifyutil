acf1c9039796b2b21ccab7b87ed6452ec9425f3b
Vitty <vittyz@gmail.com>
2018-06-12 16:19:22 +0700

    add line util

---
+++ sendline.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#echo off
+
+#penguin = PoSpTbUWQ2QYOGYyodwp6ZCJNKME0pI2GhD9RGHzwEv
+#alert = qgWH0Igvq1VZlnEuihHpObhuIh14cR7JPSNbB44pqkB
+
+export alert=iZZ4LonZTgiEtHpPON2BaUtIch7svdKlmygoetIz3nW
+export sos=PiaS6DPVBtYxS03bl3P3l8UPSpJSpswa4k3m7NgzW12
+
+
+
+export message="$1"
+export mode="$2"
+
+export bearer=UKhtBSPHMuazRlVgYCGgHdlOTKUCkz5g6aIwYKsE2iL
+
+
+
+
+
+if [ $# = "1" ]
+then
+
+  echo "Normal Mode"
+  curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer $bearer"  -d "message=$1" https://notify-api.line.me/api/notify
+
+
+elif  [ $# = "2" ]
+then
+  if [ $mode = "alert" ]
+  then
+        #echo "alert"
+     bearer=$sos
+
+     curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer $bearer"  -d "message=$1" -d "stickerPackageId=1" -d "stickerId=111" https://notify-api.line.me/api/notify
+
+  fi
+fi
+
+
+
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer $bearer"  -d "message=$1" https://notify-api.line.me/api/notify
+
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer $bearer"  -d "message=$1" -d "stickerPackageId=1" -d "stickerId=111" https://notify-api.line.me/api/notify
+
+
